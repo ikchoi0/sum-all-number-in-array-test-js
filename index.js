45356faf8e1763af45f23d897ad03b1611b581a1
Ik Sung Choi <ikchoi0@gmail.com>
2022-05-07 20:24:09 -0400

    Sum Array of Numbers

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  return array.reduce((prev, curr) => {
+    if (Array.isArray(curr)) {
+      curr = sumItems(curr);
+    }
+    return prev + curr;
+  }, 0);
 }
 
 module.exports = sumItems;
